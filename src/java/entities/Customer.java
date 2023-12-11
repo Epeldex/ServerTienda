bc305906c496e60340462dd8f14f1a7361db1e55
2dam <2dam@TartangaLH.eus>
2023-12-11 13:14:44 +0100

    creación de tres clases y declaración de atributos & getters/setters

--- src/java/entities/Customer.java
+++ src/java/entities/Customer.java
@@ -1,14 +1,160 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entities;
 
 /**
- *
- * @author alexa
+ * The Customer class represents a customer entity, extending the User class,
+ * and encapsulates information about a customer, such as their full name, email,
+ * address, phone number, and balance.
+ * 
+ * @author alexIrusta
  */
 public class Customer extends User {
+    
+    // The full name of the customer
+    private String fullName;
+    
+    // The email address of the customer
+    private String email;
+    
+    // The street address of the customer
+    private String street;
+    
+    // The postal code of the customer's address
+    private Integer postalCode;
+    
+    // The city of the customer's address
+    private String city;
+    
+    // The phone number of the customer
+    private String phone;
+    
+    // The balance of the customer's account
+    private Double balance;
+
+    /**
+     * Gets the full name of the customer.
+     * 
+     * @return The full name of the customer.
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * Sets the full name of the customer.
+     * 
+     * @param fullName The new full name of the customer.
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * Gets the email address of the customer.
+     * 
+     * @return The email address of the customer.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the email address of the customer.
+     * 
+     * @param email The new email address of the customer.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Gets the street address of the customer.
+     * 
+     * @return The street address of the customer.
+     */
+    public String getStreet() {
+        return street;
+    }
+
+    /**
+     * Sets the street address of the customer.
+     * 
+     * @param street The new street address of the customer.
+     */
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    /**
+     * Gets the postal code of the customer's address.
+     * 
+     * @return The postal code of the customer's address.
+     */
+    public Integer getPostalCode() {
+        return postalCode;
+    }
+
+    /**
+     * Sets the postal code of the customer's address.
+     * 
+     * @param postalCode The new postal code of the customer's address.
+     */
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    /**
+     * Gets the city of the customer's address.
+     * 
+     * @return The city of the customer's address.
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * Sets the city of the customer's address.
+     * 
+     * @param city The new city of the customer's address.
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * Gets the phone number of the customer.
+     * 
+     * @return The phone number of the customer.
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * Sets the phone number of the customer.
+     * 
+     * @param phone The new phone number of the customer.
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * Gets the balance of the customer's account.
+     * 
+     * @return The balance of the customer's account.
+     */
+    public Double getBalance() {
+        return balance;
+    }
+
+    /**
+     * Sets the balance of the customer's account.
+     * 
+     * @param balance The new balance of the customer's account.
+     */
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+    
     
 }
--- src/java/entities/ProductsBought.java
+++ src/java/entities/ProductsBought.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entities;
 
+import java.util.Date;
+
 /**
- *
- * @author alexa
+ * The ProductsBought class represents information about products that were bought,
+ * including the amount purchased and the timestamp of the purchase.
+ * 
+ * @author alexIrusta
  */
 public class ProductsBought {
-    
+
+    // The amount of products bought
+    private Integer amount;
+
+    // The timestamp indicating when the products were bought
+    private Date boughtTimestamp;
+
+    /**
+     * Gets the amount of products bought.
+     * 
+     * @return The amount of products bought.
+     */
+    public Integer getAmount() {
+        return amount;
+    }
+
+    /**
+     * Sets the amount of products bought.
+     * 
+     * @param amount The new amount of products bought.
+     */
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * Gets the timestamp of when the products were bought.
+     * 
+     * @return The timestamp of the purchase.
+     */
+    public Date getBoughtTimestamp() {
+        return boughtTimestamp;
+    }
+
+    /**
+     * Sets the timestamp of when the products were bought.
+     * 
+     * @param boughtTimestamp The new timestamp of the purchase.
+     */
+    public void setBoughtTimestamp(Date boughtTimestamp) {
+        this.boughtTimestamp = boughtTimestamp;
+    }
 }
