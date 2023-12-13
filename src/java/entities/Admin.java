e9ed701aa6b35826c87d036c2e99feb8720152c2
optickx <danielbarrios2002@gmail.com>
2023-12-13 11:46:52 +0100

    dani's entities finished

--- src/java/entities/Admin.java
+++ src/java/entities/Admin.java
@@ -5,10 +5,39 @@
  */
 package entities;
 
+import java.time.LocalDate;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
- * @author alexa
+ * @author dani
  */
+@XmlRootElement(name = "admin")
 public class Admin extends User {
+    private LocalDate lastAccess;
+
+
+    // getters
+    public LocalDate getLastAccess() {
+        return lastAccess;
+    }
+    // setters
+    public void setLastAccess(LocalDate lastAccess) {
+        this.lastAccess = lastAccess;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Admin)) 
+            return false;
+        
+        User other = (Admin) object;
+        if ((super.id == null && other.id != null) || (super.id != null && !super.id.equals(other.id)))
+            return false;
+        
+        return true;
+    }
+
     
 }
--- src/java/entities/User.java
+++ src/java/entities/User.java
@@ -5,10 +5,78 @@
  */
 package entities;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
- * @author alexa
+ * @author dani
  */
+@XmlRootElement(name = "user")
 public class User {
+    protected Integer id;
+    private String username;
+    private String password;
+    private boolean active;
+    private UserType userType;
+
+
+    // getters
+    public Integer getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public UserType getUserType() {
+        return userType;
+    }
+    public boolean isActive() {
+        return active;
+    }
+
+    // setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
     
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof User)) 
+            return false;
+        
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)))
+            return false;
+        
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Album[ id=" + id + " ]";
+    }
 }
