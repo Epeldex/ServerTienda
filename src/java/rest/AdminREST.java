10b0e4d63163435b745fe4a4704636c554c71dcd
optickx <danielbarrios2002@gmail.com>
2023-12-15 13:08:30 +0100

    finally rest of users added

---
+++ src/java/rest/AdminREST.java
@@ -0,0 +1,90 @@
+package rest;
+
+import ejb.IAdminManager;
+import entities.Admin;
+import exceptions.CreateException;
+import exceptions.DeleteException;
+import exceptions.ReadException;
+import exceptions.UpdateException;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Path("admins")
+public class AdminREST {
+
+    private static final Logger LOGGER = Logger.getLogger("rest");
+
+    @EJB
+    private IAdminManager ejb;
+
+    @PUT
+    @Path("updateLastAccess/{id}/{date}")
+    public void updateLastAccess(@PathParam("id") Integer id, @PathParam("date") String dateString) {
+        try {
+            LocalDate date = LocalDate.parse(dateString);
+            LOGGER.log(Level.INFO, "AdminRESTful service: Updating last access of admin; id={0}, date={1}.", new Object[]{id, date});
+            ejb.updateLastAccess(id, date);
+        } catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, "AdminRESTful service: Exception updating last access of admin, {0}", ex.getMessage());
+            throw new InternalServerErrorException(ex);
+        }
+    }
+
+    @POST
+    @Path("signin")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Admin signIn(Admin admin) {
+        try {
+            LOGGER.info("AdminRESTful service: Signing in admin.");
+            return ejb.signIn(admin.getUsername(), admin.getPassword());
+        } catch (ReadException ex) {
+            LOGGER.log(Level.SEVERE, "AdminRESTful service: Exception signing in admin, {0}", ex.getMessage());
+            throw new InternalServerErrorException(ex);
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void createAdmin(Admin admin) {
+        try {
+            LOGGER.log(Level.INFO, "AdminRESTful service: Creating new admin {0}.", admin);
+            ejb.createAdmin(admin);
+        } catch (CreateException ex) {
+            LOGGER.log(Level.SEVERE, "AdminRESTful service: Exception creating admin, {0}", ex.getMessage());
+            throw new InternalServerErrorException(ex);
+        }
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void updateAdmin(Admin admin) {
+        try {
+            LOGGER.log(Level.INFO, "AdminRESTful service: Updating admin {0}.", admin);
+            ejb.updateAdmin(admin);
+        } catch (UpdateException ex) {
+            LOGGER.log(Level.SEVERE, "AdminRESTful service: Exception updating admin, {0}", ex.getMessage());
+            throw new InternalServerErrorException(ex);
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void removeAdmin(@PathParam("id") Integer id) {
+        try {
+            LOGGER.log(Level.INFO, "AdminRESTful service: Removing admin by id={0}.", id);
+            ejb.removeAdmin(id);
+        } catch (DeleteException ex) {
+            LOGGER.log(Level.SEVERE, "AdminRESTful service: Exception removing admin, {0}", ex.getMessage());
+            throw new InternalServerErrorException(ex);
+        }
+    }
+
+    // Additional methods can be added here as needed.
+
+}
